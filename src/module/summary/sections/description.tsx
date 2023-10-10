b2323e8a88a10af28ad34ab9ea53f1b14c221ce0
fiqriardiansyah <fiqriardian92@gmail.com>
2023-10-10 21:08:13 +0700

    feat: add cv download

--- src/module/summary/sections/description.tsx
+++ src/module/summary/sections/description.tsx
@@ -1,6 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
+import { motion } from 'framer-motion'
+import StaggerElementFooter from 'module/footer/stagger-element'
 import React from 'react'
+
 const Description = () => {
+  const [hover, setHover] = React.useState(false)
+
   return (
     <div className="CONTAINER min-h-screen">
       <h1 className="font-display text-6xl font-semibold text-secondary underline lg:text-8xl">Summary</h1>
@@ -16,6 +21,37 @@ const Description = () => {
           <br />
           Let's make something beautiful with code! 🎉🎉🎉
         </p>
+        <a href={`/fiqriardiansyah_cv.pdf`} target="_blank" download title="download cv" className="mt-10 w-fit" rel="noreferrer">
+          <motion.div
+            onHoverStart={() => setHover(true)}
+            onHoverEnd={() => setHover(false)}
+            className="relative rounded border border-white px-5 py-2 font-poppins capitalize text-white"
+          >
+            <span className="text-xl opacity-0">download cv</span>
+            <StaggerElementFooter
+              aria-hidden
+              to="0"
+              triger={hover}
+              from="-130%"
+              perLetter
+              tag="h2"
+              className="pointer-events-none absolute left-1/2 top-1/2 z-10 m-0 -translate-x-1/2 -translate-y-1/2 whitespace-nowrap font-display text-xl font-medium leading-[1.3] text-white"
+            >
+              Download CV
+            </StaggerElementFooter>
+            <StaggerElementFooter
+              aria-hidden
+              to="130%"
+              from="0"
+              triger={hover}
+              perLetter
+              tag="h2"
+              className="pointer-events-none absolute left-1/2 top-1/2 z-10 m-0 -translate-x-1/2 -translate-y-1/2 whitespace-nowrap font-display text-xl font-medium leading-[1.3] text-white"
+            >
+              Download CV
+            </StaggerElementFooter>
+          </motion.div>
+        </a>
       </div>
     </div>
   )
