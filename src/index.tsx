3a2e21303bc535cae6e74b81a7bc2101c9eb22d3
fiqriardiansyah <fiqriardian92@gmail.com>
2023-10-20 23:24:56 +0700

    add analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import App from 'app'
+import { Analytics } from '@vercel/analytics/react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './styles/globals.css'
@@ -8,6 +9,7 @@ const root = createRoot(container)
 
 root.render(
   <BrowserRouter>
+    <Analytics />
     <Routes>
       <Route path="/*" element={<App />} />
     </Routes>
