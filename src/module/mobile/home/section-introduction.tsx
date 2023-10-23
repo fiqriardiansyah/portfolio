3fba5b6cd1ec40a24f0b278c529f7dfaa2268420
fiqriardiansyah <fiqriardian92@gmail.com>
2023-10-23 10:19:45 +0700

    new cv, change section intro

--- src/module/mobile/home/section-introduction.tsx
+++ src/module/mobile/home/section-introduction.tsx
@@ -1,34 +1,20 @@
-import StaggerSlideElementHome from 'module/home/stagger-slide-element'
-
 export default function SectionIntroductionMobile() {
   return (
-    <div className="CONTAINER">
-      <div className="flex h-screen w-full flex-col items-center justify-center">
-        <StaggerSlideElementHome
-          className="MENU-CHANGE-Y-200 text-center font-display text-6xl leading-[1.3] text-secondary md:text-8xl xl:text-9xl 2xl:text-[10rem]"
-          perLetter={false}
-          tag="h1"
-        >
+    <div className="CONTAINER snap-y snap-mandatory">
+      <div className="flex h-screen w-full snap-center flex-col items-center justify-center">
+        <h1 className="MENU-CHANGE-Y-200 text-center font-display text-6xl leading-[1.3] text-secondary md:text-8xl xl:text-9xl 2xl:text-[10rem]">
           Let Me Introduce My Self As Developer
-        </StaggerSlideElementHome>
+        </h1>
       </div>
-      <div className="flex h-screen w-full flex-col items-center justify-center">
-        <StaggerSlideElementHome
-          className="MENU-CHANGE-Y-200 text-center  font-display text-6xl leading-[1.3] text-secondary md:text-8xl xl:text-9xl 2xl:text-[10rem]"
-          perLetter={false}
-          tag="h1"
-        >
+      <div className="flex h-screen w-full snap-center flex-col items-center justify-center">
+        <h1 className="MENU-CHANGE-Y-200 text-center  font-display text-6xl leading-[1.3] text-secondary md:text-8xl xl:text-9xl 2xl:text-[10rem]">
           Creative Developer Based In Indonesia
-        </StaggerSlideElementHome>
+        </h1>
       </div>
-      <div className="flex h-screen w-full flex-col items-center justify-center">
-        <StaggerSlideElementHome
-          className="MENU-CHANGE-Y-200 text-center  font-display text-6xl leading-[1.3] text-secondary md:text-8xl xl:text-9xl 2xl:text-[10rem]"
-          perLetter={false}
-          tag="h1"
-        >
+      <div className="flex h-screen w-full snap-center flex-col items-center justify-center">
+        <h1 className="MENU-CHANGE-Y-200 text-center  font-display text-6xl leading-[1.3] text-secondary md:text-8xl xl:text-9xl 2xl:text-[10rem]">
           Specializing In Web Front-end Development
-        </StaggerSlideElementHome>
+        </h1>
       </div>
     </div>
   )
