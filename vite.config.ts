c077b45a9e4a6f1fce36f0127038fdcaf008f49e
Ubuntu <ubuntu@ip-172-31-47-6.ap-southeast-2.compute.internal>
2024-01-12 03:52:28 +0000

    add changes on host/port

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
+  server: {
+   port: 9000,
+  },
+  preview: {
+ port: 9000,
+ host: import.meta.env.MODE === 'production' ? '0.0.0.0' : 'localhost',
+},
   plugins: [react(), tsconfigPaths()],
   build: {
     rollupOptions: {
